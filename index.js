8c9598003185914d9298ea7caf2566dd6c6f326c
Exerchan <58511942+Exerchan@users.noreply.github.com>
2021-01-26 19:15:38 +0100

    Added token

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-    });
-
-client.on('message', msg => {
-    if (msg.content === 'ping') {
-    msg.reply('pong');
-    }
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.login('Set Key');
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("pong");
+  }
+});
+
+client.login("ODAzNjYzODg3MzgzMTM0MjE4.YBBEUg.y23sS5sjktFL-f9XOMoA7thcMBY");
