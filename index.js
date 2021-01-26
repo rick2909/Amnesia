f63ca2a90083ee51f029efa95abba0290ac2c17c
Exerchan <58511942+Exerchan@users.noreply.github.com>
2021-01-26 20:22:39 +0100

    Added prefix, ping and unknown command

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+const Prefix = "~";
+
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on("message", (msg) => {
-  if (msg.content === "ping") {
-    msg.reply("pong");
+  if (msg.content.substring(0, 1) === Prefix) {
+    let message = msg.content.substring(1, msg.content.length);
+    if (message === "ping") {
+      msg.reply("pong");
+      return;
+    }
+
+    msg.reply("Unknown command. Typ ~help for more information.");
   }
 });
 
