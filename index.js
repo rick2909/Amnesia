147093733a013c638a595e36f432b694a1e3dda8
rick2909 <rick29998@gmail.com>
2021-01-27 09:53:31 +0100

    Settup js layout

--- index.js
+++ index.js
@@ -1,14 +1,63 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const Discord = require("discord.js");
+const dotenv = require("dotenv");
+const BotConfig = require("./config.json");
+const fs = require("fs");
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-    });
+dotenv.config();
 
-client.on('message', msg => {
-    if (msg.content === 'ping') {
-    msg.reply('pong');
-    }
+const Prefix = BotConfig.prefix;
+const bot = new Discord.Client();
+
+bot.commands = new Discord.Collection();
+bot.aliases = new Discord.Collection();
+
+fs.readdir("./commands/", (err, files) => {
+	if(err) console.log(err);
+
+	const jsfile = files.filter(f => f.split(".").pop() === "js");
+	if(jsfile.length <= 0) {
+		console.log("No commands found!");
+	}
+
+	jsfile.forEach((f) => {
+		const props = require(`./commands/${f}`);
+		console.log(`${f} loaded!`);
+		bot.commands.set(props.help.name, props);
+
+		props.help.aliases.forEach(alias => {
+			bot.aliases.set(alias, props.help.name);
+		});
+	});
 });
 
-client.login('Set Key');
+bot.on("ready", () => {
+	console.log(`${bot.user.tag} is Online on ${bot.guilds.cache.size} servers!`);
+	bot.user.setActivity(`I'm on ${bot.guilds.cache.size} servers!`);
+});
+
+bot.on("message", async msg => {
+	// disable DM
+	if(msg.channel.type === "dm") return;
+	// disable responding to it self
+	if(msg.author.id === bot.user.id) return;
+
+	if (!msg.content.startsWith(Prefix)) return;
+
+	const args = msg.content.slice(Prefix.length).trim().split(/ +/g);
+	const cmd = args.shift().toLowerCase();
+	let command;
+
+	if(bot.commands.has(cmd)) {
+		command = bot.commands.get(cmd);
+	} else if(bot.aliases.has(cmd)) {
+		command = bot.commands.get(bot.aliases.get(cmd));
+	}
+
+	try {
+		command.run(bot, msg, args);
+	} catch(e) {
+		return;
+	}
+});
+
+bot.login(process.env.TOKEN);
