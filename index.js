3f2d88c383d2f1f34000c7b3e5ad41a0088e813d
Exerchan <58511942+Exerchan@users.noreply.github.com>
2021-01-27 01:16:26 +0100

    alarm function does not work, added welcome function

--- index.js
+++ index.js
@@ -1,22 +1,66 @@
 const Discord = require("discord.js");
+const { setTimeout } = require("timers");
 const client = new Discord.Client();
+const Token = "ODAzNjYzODg3MzgzMTM0MjE4.YBBEUg.y23sS5sjktFL-f9XOMoA7thcMBY";
 
 const Prefix = "~";
+const ErrorMessage = "Unknown command. Typ ~help for more information.";
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("guildMemberAdd", (member) => {
+  const channel = member.guild.channels.cache.find(
+    (ch) => ch.name === "general"
+  );
+  if (!channel) return;
+  channel.send(`Welcome to hell, ${member}`);
 });
 
 client.on("message", (msg) => {
   if (msg.content.substring(0, 1) === Prefix) {
     let message = msg.content.substring(1, msg.content.length);
     if (message === "ping") {
-      msg.reply("pong");
+      msg.react("🏓").then(msg.reply("pong")).catch(console.error);
+      return;
+    }
+    if (message.startsWith("alarm")) {
+      alarm(msg);
+      return;
+    }
+    if (message === "avatar") {
+      msg.reply(msg.author.displayAvatarURL()).catch(console.error);
       return;
     }
 
-    msg.reply("Unknown command. Typ ~help for more information.");
+    msg.reply(ErrorMessage);
   }
 });
 
-client.login("ODAzNjYzODg3MzgzMTM0MjE4.YBBEUg.y23sS5sjktFL-f9XOMoA7thcMBY");
+function alarm(msg) {
+  msg.content = msg.content.substring(6, msg.content.length);
+  let pivot = getAlarmPivot(msg.content);
+  if (pivot != -1) {
+    setTimeout(function () {
+      msg.channel.send(msg.content(pivot + 1, msg.content.length)),
+        parseInt(msg.content.substring(0, pivot));
+    });
+    return;
+  }
+
+  msg.channel.send(
+    "Incorrect use of the alarm command: ~alarm seconds message"
+  );
+}
+
+function getAlarmPivot(message) {
+  for (let i = 0; i < message.length - 1; i++) {
+    if (message.substring(i, i + 1)) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+client.login(Token);
