22f128bb98136b85e05665c4ffa6d5076cb6adc5
Exerchan <58511942+Exerchan@users.noreply.github.com>
2021-01-28 15:03:48 +0100

    did what nazibot told me to do

--- commands/alarm.js
+++ commands/alarm.js
@@ -1,60 +1,56 @@
 module.exports.run = async (bot, message, args) => {
-  const alarmFormats = [
-    ["s", "second(s)", 1000],
-    ["m", "minute(s)", 60000],
-    ["h", "hour(s)", 360000],
-    ["d", "day(s)", 8640000],
-    ["w", "week(s)", 60480000],
-    ["M", "month(s)", 259200000],
-  ];
+	const alarmFormats = [
+		["s", "second(s)", 1000],
+		["m", "minute(s)", 60000],
+		["h", "hour(s)", 360000],
+		["d", "day(s)", 8640000],
+		["w", "week(s)", 60480000],
+		["M", "month(s)", 259200000],
+	];
 
-  let messageContent = await message.content.substring(7);
-  let pivot = getPivot(messageContent);
-  if (pivot == -1) {
-    message.channel.send(
-      "Incorrect use of alarm command: ~alarm [amount][format s|m|h|d|w|M] [message]"
-    );
-  }
+	const messageContent = await message.content.substring(7);
+	const pivot = getPivot(messageContent);
+	if (pivot == -1) {
+		message.channel.send("Incorrect use of alarm command: ~alarm [amount][format s|m|h|d|w|M] [message]");
+	}
 
-  let time = messageContent.substring(0, pivot);
-  let alarmMessage = messageContent.substring(pivot + 1);
+	const time = messageContent.substring(0, pivot);
+	const alarmMessage = messageContent.substring(pivot + 1);
 
-  let formatIndex = getFormat(time, alarmFormats);
-  if (formatIndex == -1) {
-    message.channel.send(
-      "Incorrect use of alarm command: ~alarm [amount][format s|m|h|d|w|M] [message]"
-    );
-  }
-  const msg = await message.reply(
-    `alarm set for ${time.substring(0, time.length - 1)} ${
-      alarmFormats[formatIndex][1]
-    } with message: ${alarmMessage}`
-  );
-  const alarm = await setTimeout(function () {
-    message.reply(alarmMessage);
-  }, time.substring(0, time.length - 1) * alarmFormats[formatIndex][2]);
+	const formatIndex = getFormat(time, alarmFormats);
+	if (formatIndex == -1) {
+		message.channel.send("Incorrect use of alarm command: ~alarm [amount][format s|m|h|d|w|M] [message]");
+	}
+	const msg = await message.reply(
+		`alarm set for ${time.substring(0, time.length - 1)} ${
+			alarmFormats[formatIndex][1]
+		} with message: ${alarmMessage}`,
+	);
+	const alarm = await setTimeout(function() {
+		message.reply(alarmMessage);
+	}, time.substring(0, time.length - 1) * alarmFormats[formatIndex][2]);
 };
 
 module.exports.help = {
-  name: "alarm",
-  aliases: ["a"],
+	name: "alarm",
+	aliases: ["a"],
 };
 
 function getPivot(message) {
-  for (let i = 0; i < message.length - 1; i++) {
-    if (message.substring(i, i + 1) === " ") {
-      return i;
-    }
-  }
-  return -1;
+	for (let i = 0; i < message.length - 1; i++) {
+		if (message.substring(i, i + 1) === " ") {
+			return i;
+		}
+	}
+	return -1;
 }
 
 function getFormat(time, formats) {
-  timeFormat = time.substring(time.length - 1);
-  for (let i = 0; i < formats.length; i++) {
-    if (timeFormat === formats[i][0]) {
-      return i;
-    }
-  }
-  return -1;
+	const timeFormat = time.substring(time.length - 1);
+	for (let i = 0; i < formats.length; i++) {
+		if (timeFormat === formats[i][0]) {
+			return i;
+		}
+	}
+	return -1;
 }
--- index.js
+++ index.js
@@ -12,54 +12,52 @@ bot.commands = new Discord.Collection();
 bot.aliases = new Discord.Collection();
 
 fs.readdir("./commands/", (err, files) => {
-  if (err) console.log(err);
+	if (err) console.log(err);
 
-  const jsfile = files.filter((f) => f.split(".").pop() === "js");
-  if (jsfile.length <= 0) {
-    console.log("No commands found!");
-  }
+	const jsfile = files.filter((f) => f.split(".").pop() === "js");
+	if (jsfile.length <= 0) {
+		console.log("No commands found!");
+	}
 
-  jsfile.forEach((f) => {
-    const props = require(`./commands/${f}`);
-    console.log(`${f} loaded!`);
-    bot.commands.set(props.help.name, props);
+	jsfile.forEach((f) => {
+		const props = require(`./commands/${f}`);
+		console.log(`${f} loaded!`);
+		bot.commands.set(props.help.name, props);
 
-    props.help.aliases.forEach((alias) => {
-      bot.aliases.set(alias, props.help.name);
-    });
-  });
+		props.help.aliases.forEach((alias) => {
+			bot.aliases.set(alias, props.help.name);
+		});
+	});
 });
 
 bot.on("ready", () => {
-  console.log(`${bot.user.tag} is Online on ${bot.guilds.cache.size} servers!`);
-  bot.user.setActivity(
-    `I'm on ${bot.guilds.cache.size} servers! In Alpha state`
-  );
+	console.log(`${bot.user.tag} is Online on ${bot.guilds.cache.size} servers!`);
+	bot.user.setActivity(`I'm on ${bot.guilds.cache.size} servers! In Alpha state`);
 });
 
 bot.on("message", async (msg) => {
-  // disable DM
-  if (msg.channel.type === "dm") return;
-  // disable responding to it self
-  if (msg.author.id === bot.user.id) return;
+	// disable DM
+	if (msg.channel.type === "dm") return;
+	// disable responding to it self
+	if (msg.author.id === bot.user.id) return;
 
-  if (!msg.content.startsWith(Prefix)) return;
+	if (!msg.content.startsWith(Prefix)) return;
 
-  const args = msg.content.slice(Prefix.length).trim().split(/ +/g);
-  const cmd = args.shift().toLowerCase();
-  let command;
+	const args = msg.content.slice(Prefix.length).trim().split(/ +/g);
+	const cmd = args.shift().toLowerCase();
+	let command;
 
-  if (bot.commands.has(cmd)) {
-    command = bot.commands.get(cmd);
-  } else if (bot.aliases.has(cmd)) {
-    command = bot.commands.get(bot.aliases.get(cmd));
-  }
+	if (bot.commands.has(cmd)) {
+		command = bot.commands.get(cmd);
+	} else if (bot.aliases.has(cmd)) {
+		command = bot.commands.get(bot.aliases.get(cmd));
+	}
 
-  try {
-    command.run(bot, msg, args);
-  } catch (e) {
-    return;
-  }
+	try {
+		command.run(bot, msg, args);
+	} catch (e) {
+		return;
+	}
 });
 
 bot.login(process.env.TOKEN);
