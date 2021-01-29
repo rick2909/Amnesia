e4e4b0f79a609a528167fbbe148f8460aaa850d4
Exerchan <58511942+Exerchan@users.noreply.github.com>
2021-01-29 01:10:50 +0100

    tried adding music to it

--- commands/goodnight.js
+++ commands/goodnight.js
@@ -5,10 +5,9 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 	const goodnightEmbed = new Discord.MessageEmbed()
 		.setColor("#0099ff")
-		.setTitle("Goodnight girl, see you tomorrow");
-	const gif = new Discord.MessageAttachment("Assets/command/goodnight.gif");
-	message.channel.send({embed: goodnightEmbed, files: [gif]});
-
+		.setTitle("Goodnight girl, see you tomorrow")
+		.attachFiles(new Discord.MessageAttachment("Assets/command/goodnight.gif"));
+	message.channel.send({embed: goodnightEmbed});
 };
 
 module.exports.help = {
